2b4a44db6a285e18344e7f3251985cdc930a0fa4
Daping Guo <daping.guo@qq.com>
2018-04-16 19:41:35 +0800

    Update TEditText.kt

--- wallet/src/main/kotlin/org/trustnote/wallet/widget/TEditText.kt
+++ wallet/src/main/kotlin/org/trustnote/wallet/widget/TEditText.kt
@@ -1,14 +1,14 @@
 package org.trustnote.wallet.widget
 
-import android.widget.EditText
 import android.content.Context
+import android.support.v7.widget.AppCompatTextView
 import android.util.AttributeSet
 
 class TEditText @JvmOverloads constructor(
         context: Context,
         attrs: AttributeSet? = null,
         defStyleAttr: Int = 0
-) : EditText(context, attrs, defStyleAttr) {
+) : AppCompatTextView(context, attrs, defStyleAttr) {
 
 }
 
