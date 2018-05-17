b1c9b818fa561a970f1787de8b9e4daa8d2cfe76
guo <guodaping@gmail.com>
2018-05-17 17:14:21 +0800

    Build: 23

--- wallet/src/main/kotlin/org/trustnote/wallet/FragmentInputPwd.kt
+++ wallet/src/main/kotlin/org/trustnote/wallet/FragmentInputPwd.kt
@@ -9,7 +9,7 @@ import org.trustnote.wallet.widget.InputPwdDialogFragment
 
 @SuppressLint("ValidFragment")  //TODO: the fragment cannot re-create from tomb.
 open class FragmentInputPwd(layoutId: Int) : CreateWalletFragment(layoutId) {
-    
+
     override fun initFragment(view: View) {
         view.findViewById<View>(R.id.pwd_exist_clickcontinue).setOnClickListener {
             InputPwdDialogFragment.showMe(activity, {
